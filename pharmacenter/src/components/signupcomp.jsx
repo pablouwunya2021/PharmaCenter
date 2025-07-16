9f9bf7d19acd1074626e6c783d94bd3b96569fad
Estuardo Castro <cas23890@uvg.edu.gt>
2025-07-16 15:21:29 -0600

    Cambiar logo, crear componente y vista signup

---
+++ pharmacenter/src/components/signupcomp.jsx
@@ -0,0 +1,9 @@
+const signupcomp = () => {
+  return (
+    <div classname="signup">
+        <h1>Componente para crear usuario</h1>
+    </div>
+  );
+}
+
+export default signupcomp;
---
+++ pharmacenter/src/pages/signup.jsx
@@ -0,0 +1,12 @@
+import signupcomp from "../components/signupcomp";
+
+function signup() {
+  return (
+    <div>
+        <h1>Página para crear usuario</h1>
+        <signupcomp />
+    </div>
+  );
+}
+
+export default signup;
