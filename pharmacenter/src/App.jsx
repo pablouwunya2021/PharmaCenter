d01c0d3b14ec867c579b906549faf58ca35579b8
Josero31 <san231221@uvg.edu.gt>
2025-07-19 14:57:25 -0600

    feat: agregar ruta /login al sistema de rutas
    - Importa LoginPage en App.jsx
    - Configura ruta /login en React Router
    - Permite acceso directo a la página de login
    - Integra el login al flujo de navegación de la aplicación

--- pharmacenter/src/App.jsx
+++ pharmacenter/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Signup from './pages/signup'
 import Cart from './pages/cart';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   )
 }
