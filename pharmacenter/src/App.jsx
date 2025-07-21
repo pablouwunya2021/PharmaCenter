f3ae2e34dc2c1c97bde9a58eeac15a03e1f1bd9a
angel <mer23661@uvg.edu.gt>
2025-07-21 16:27:07 -0600

    Añadir pagina al App.,jsx

--- pharmacenter/src/App.jsx
+++ pharmacenter/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <Route path="/cart" element={<Cart />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/compra/:id" element={<CompraPage />} />
+      <Route path="/inventory" element={<Inventory />} /> 
     </Routes>
   )
 }
