85f566efeb65cac239f0efd873c441510f8f73c5
can23384 <can23384@uvg.edu.gt>
2025-07-20 19:59:52 -0600

    page de compra

---
+++ pharmacenter/src/pages/CompraPage.jsx
@@ -0,0 +1,41 @@
+// src/pages/CompraPage.jsx
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ProductoCard from '../components/ProductoCard';
+import HeaderCompraPage from '../components/HeaderCompraPage'; // Al inicio del archivo
+
+function CompraPage() {
+  const { id } = useParams();
+  const [producto, setProducto] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function cargarProducto() {
+      try {
+        const res = await fetch(`http://localhost:3000/api/medicamentos/${id}`);
+        if (!res.ok) throw new Error('No se pudo obtener el medicamento');
+        const data = await res.json();
+        setProducto(data);
+      } catch (err) {
+        setError(err.message);
+      }
+    }
+
+    cargarProducto();
+  }, [id]);
+
+  if (error) return <p>Error: {error}</p>;
+  if (!producto) return <p>Cargando...</p>;
+
+  return (
+  <>
+    <HeaderCompraPage />
+    <main style={{ padding: '2rem' }}>
+      <ProductoCard producto={producto} />
+    </main>
+  </>
+);
+
+}
+
+export default CompraPage;
