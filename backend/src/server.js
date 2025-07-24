705236282ba46078a6adf0aec2db1c2a8960c424
Josero31 <san231221@uvg.edu.gt>
2025-07-23 22:42:28 -0600

    Agreagar Header para estar presente en todas las paginas principales excepto  donde se incluya manualente

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`API PharmaCenter corriendo en http://localhost:${PORT}`);
+  console.log(`API PharmaCenter en http://localhost:${PORT}`);
 });
--- pharmacenter/src/App.jsx
+++ pharmacenter/src/App.jsx
@@ -5,16 +5,41 @@ import Cart from './pages/cart';
 import LoginPage from './pages/LoginPage';
 import CompraPage from './pages/CompraPage';
 import Inventory from './pages/Inventory'
+import Header from './components/Header'
+
+// Layout que incluye el Header
+const MainLayout = ({ children }) => (
+  <>
+    <Header />
+    {children}
+  </>
+);
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/compra/:id" element={<CompraPage />} />
-      <Route path="/inventory" element={<Inventory />} /> 
+      <Route path="/cart" element={
+        <MainLayout>
+          <Cart />
+        </MainLayout>
+      } />
+      <Route path="/login" element={
+        <MainLayout>
+          <LoginPage />
+        </MainLayout>
+      } />
+      <Route path="/compra/:id" element={
+        <MainLayout>
+          <CompraPage />
+        </MainLayout>
+      } />
+      <Route path="/inventory" element={
+        <MainLayout>
+          <Inventory />
+        </MainLayout>
+      } />
     </Routes>
   )
 }
