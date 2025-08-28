d3fdbc744e7ceed8b35c6197729b3e9c9b33b75f
can23384 <can23384@uvg.edu.gt>
2025-08-27 19:01:36 -0600

    añadir espacio de renombrar

--- pharmacenter/src/components/InventoryTable.jsx
+++ pharmacenter/src/components/InventoryTable.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
 import InventoryItem from './InventoryItem'
 
-const InventoryTable = ({ items }) => (
-  <div className="card">
-    <table className="inventory-table">
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Cantidad</th>
-          <th>Vencimiento</th>
-          <th>Precio</th>
-          <th>Costo</th>
-          <th>Proveedor</th>
-          <th>Imagen</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map(item => (
-          <InventoryItem key={item.idmedicamento || item.nombre} item={item} />
-        ))}
-      </tbody>
-    </table>
-  </div>
-)
+const InventoryTable = ({ items, onChanged }) => {
+  return (
+    <div className="card">
+      <table className="inventory-table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Cantidad</th>
+            <th>Vencimiento</th>
+            <th>Precio</th>
+            <th>Costo</th>
+            <th>Proveedor</th>
+            <th>Imagen</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(item => (
+            <InventoryItem
+              key={item.idmedicamento || item.nombre}
+              item={item}
+              onChanged={onChanged}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 export default InventoryTable
