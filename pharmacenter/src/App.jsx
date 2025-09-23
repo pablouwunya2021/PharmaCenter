0db7b61aec6dafa3a6a2198bfcb8f5add349551b
angel <mer23661@uvg.edu.gt>
2025-09-23 11:18:30 -0600

    reorganizar rutas e integrar AdminLayout

--- pharmacenter/src/App.jsx
+++ pharmacenter/src/App.jsx
@@ -1,13 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Signup from './pages/signup'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/signup';
 import Cart from './pages/cart';
 import LoginPage from './pages/LoginPage';
 import CompraPage from './pages/CompraPage';
-import Inventory from './pages/Inventory'
-import Header from './components/Header'
+import Inventory from './pages/Inventory';
+import Header from './components/Header';
 
-// Layout que incluye el Header
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminLayout from './components/admin/AdminLayout';
+import AdminDashboard from './pages/admin/AdminDashboard';
+
 const MainLayout = ({ children }) => (
   <>
     <Header />
@@ -18,30 +21,27 @@ const MainLayout = ({ children }) => (
 function App() {
   return (
     <Routes>
+      {/* Público */}
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/cart" element={
-        <MainLayout>
-          <Cart />
-        </MainLayout>
-      } />
-      <Route path="/login" element={
-        <MainLayout>
-          <LoginPage />
-        </MainLayout>
-      } />
-      <Route path="/compra/:id" element={
-        <MainLayout>
-          <CompraPage />
-        </MainLayout>
-      } />
-      <Route path="/inventory" element={
-        <MainLayout>
-          <Inventory />
-        </MainLayout>
-      } />
+      <Route path="/cart" element={<MainLayout><Cart /></MainLayout>} />
+      <Route path="/login" element={<MainLayout><LoginPage /></MainLayout>} />
+      <Route path="/compra/:id" element={<MainLayout><CompraPage /></MainLayout>} />
+
+      {/* Panel Admin (solo admins) */}
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute role="admin">
+            <AdminLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<AdminDashboard />} />
+        <Route path="inventory" element={<Inventory />} />
+      </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
