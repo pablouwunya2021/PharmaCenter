dd2747295bc27d93c26c17146b3188e7e63ce6c7
angel <mer23661@uvg.edu.gt>
2025-10-09 19:31:01 -0600

    Estilizar ClearCartButton como botón de peligro

--- pharmacenter/src/components/ClearCartButton.jsx
+++ pharmacenter/src/components/ClearCartButton.jsx
@@ -5,7 +5,16 @@ import { useCart } from '../context/CartContext';
 const ClearCartButton = () => {
   const { clearCart } = useCart();
 
-  return <button onClick={clearCart}>Vaciar Carrito</button>;
+  return (
+    <button
+      type="button"
+      className="btn btn--danger btn--small"
+      onClick={clearCart}
+      aria-label="Vaciar carrito"
+    >
+      Vaciar Carrito
+    </button>
+  );
 };
 
 export default ClearCartButton;
