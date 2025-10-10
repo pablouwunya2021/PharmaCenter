1316f52ce16e84294d83a7b140f9f250a90c4746
angel <mer23661@uvg.edu.gt>
2025-10-09 19:28:13 -0600

    Modificaciones

--- pharmacenter/src/components/CartItem.jsx
+++ pharmacenter/src/components/CartItem.jsx
@@ -6,11 +6,22 @@ const CartItem = React.forwardRef(({ item }, ref) => {
   const { removeFromCart } = useCart();
 
   return (
-    <div ref={ref} style={{ borderBottom: '1px solid #ccc', padding: '1rem 0' }}>
-      <h4>{item.nombre}</h4>
-      <p>Precio: ${item.precio}</p>
-      <p>Cantidad: {item.cantidad}</p>
-      <button onClick={() => removeFromCart(item.id)}>Eliminar</button>
+    <div ref={ref} className="cart-item">
+      <div className="cart-item-info">
+        <h4 className="cart-item-title">{item.nombre}</h4>
+        <p className="cart-item-price">Precio: ${item.precio}</p>
+        <p className="cart-item-qty">Cantidad: {item.cantidad}</p>
+      </div>
+      <div className="cart-item-actions">
+        <button
+          type="button"
+          className="btn btn--ghost"
+          onClick={() => removeFromCart(item.id)}
+          aria-label={`Eliminar ${item.nombre}`}
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 });
